67c9940e1d422bbddf94a9eda1c8183e3606e84e
Shital Shah <sytelus@gmail.com>
2019-12-21 03:27:40 -0800

    fix params for new Trainer

--- FastAutoAugment/nas/test_arch.py
+++ FastAutoAugment/nas/test_arch.py
@@ -58,8 +58,8 @@ def test_arch(conf_common:Config, conf_data:Config, conf_test:Config,
 
     device = torch.device("cuda")
 
-    train_lossfn = get_lossfn(conf_train_lossfn, conf_data).to(device)
-    test_lossfn = get_lossfn(conf_test_lossfn, conf_data).to(device)
+    train_lossfn = get_lossfn(conf_train_lossfn).to(device)
+    test_lossfn = get_lossfn(conf_test_lossfn).to(device)
 
     # create model
     model = Model(model_desc)
@@ -71,9 +71,9 @@ def test_arch(conf_common:Config, conf_data:Config, conf_test:Config,
     optim = get_optimizer(conf_opt, model.parameters())
     lr_scheduler = get_lr_scheduler(conf_lr_sched, epochs, optim)
 
-    trainer = Trainer(model, train_lossfn, aux_weight, grad_clip, drop_path_prob,
-        report_freq, tb_tag='eval_train',
-        val_logger_freq=1000, val_tb_tag='eval_test')
+    trainer = Trainer(model, device, train_lossfn, test_lossfn,
+        aux_weight, grad_clip, drop_path_prob, report_freq,
+        tb_tag='eval_train', val_logger_freq=1000, val_tb_tag='eval_test')
     train_metrics, test_metrics = trainer.fit(train_dl, test_dl, epochs,
                                               optim, lr_scheduler)
     test_metrics.report_best()
