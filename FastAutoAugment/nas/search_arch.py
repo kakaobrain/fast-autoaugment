0239339ffc4ab045655952ef8cc9b65c99039225
Shital Shah <shitals@microsoft.com>
2019-12-22 19:35:21 +0000

    added cell shape test, refactor search and config to allow white box text

--- FastAutoAugment/nas/search_arch.py
+++ FastAutoAugment/nas/search_arch.py
@@ -1,52 +1,45 @@
-from typing import Tuple, Any, Iterable, Optional
-import  torch.nn as nn
+from scripts.darts.cifar_search import conf
 import torch
 import os
-import yaml
 
 from ..common.config import Config
 from .model import Model
 from ..common.data import get_dataloaders
-from ..common.common import get_logger, get_tb_writer
-from ..common.optimizer import get_lr_scheduler, get_optimizer, get_lossfn
-from .vis_genotype import draw_model_desc
-from ..common.train_test_utils import train_test
 from .model_desc import ModelDesc, RunMode
 from .model_desc_builder import ModelDescBuilder
 from .dag_mutator import DagMutator
 from .arch_trainer import ArchTrainer
 
-def search_arch(conf_common:Config, conf_data:Config, conf_search:Config,
-                dag_mutator:DagMutator, arch_trainer:ArchTrainer)->ModelDesc:
+
+def search_arch(conf_common: Config, conf_data: Config, conf_search: Config,
+                dag_mutator: DagMutator, arch_trainer: ArchTrainer) -> ModelDesc:
     logger = get_logger()
 
     # region conf vars
-    horovod       = conf_common['horovod']
-    logger_freq   = conf_common['logger_freq']
-    plotsdir      = conf_common['plotsdir']
+    horovod = conf_common['horovod']
+    logger_freq = conf_common['logger_freq']
+    plotsdir = conf_common['plotsdir']
     logdir = conf_common['logdir']
     # dataset
-    ds_name       = conf_data['name']
-    max_batches   = conf_data['max_batches']
-    dataroot      = conf_data['dataroot']
+    ds_name = conf_data['name']
+    max_batches = conf_data['max_batches']
+    dataroot = conf_data['dataroot']
     # data loader
-    conf_loader   = conf_search['loader']
-    aug           = conf_loader['aug']
-    cutout        = conf_loader['cutout']
-    val_ratio     = conf_loader['val_ratio']
-    batch_size    = conf_loader['batch']
-    epochs        = conf_loader['epochs']
-    val_fold      = conf_loader['val_fold']
-    n_workers     = conf_loader['n_workers']
+    conf_loader = conf_search['loader']
+    aug = conf_loader['aug']
+    cutout = conf_loader['cutout']
+    val_ratio = conf_loader['val_ratio']
+    batch_size = conf_loader['batch']
+    epochs = conf_loader['epochs']
+    val_fold = conf_loader['val_fold']
+    n_workers = conf_loader['n_workers']
     # search
     model_desc_file = conf_search['model_desc_file']
-    data_parallel = conf_search['data_parallel']
     conf_model_desc = conf_search['model_desc']
     # endregion
 
-    builder = ModelDescBuilder(conf_data, conf_model_desc, run_mode=RunMode.Search)
-    model_desc = builder.get_model_desc()
-    dag_mutator.mutate(model_desc)
+    device = torch.device('cuda')
+    model = create_model(conf_data, conf_model_desc, dag_mutator, device)
 
     # get data
     train_dl, val_dl, *_ = get_dataloaders(
@@ -55,20 +48,11 @@ def search_arch(conf_common:Config, conf_data:Config, conf_search:Config,
         val_ratio=val_ratio, val_fold=val_fold, horovod=horovod,
         n_workers=n_workers, max_batches=max_batches)
 
-    device = torch.device('cuda')
-
-    model = Model(model_desc)
-    # if data_parallel:
-    #     model = nn.DataParallel(model).to(device)
-    # else:
-    # TODO: enable DataParallel
-    model = model.to(device)
-
     found_model_desc, *_ = arch_trainer.fit(conf_search, model, device,
                                             train_dl, val_dl,
                                             epochs, plotsdir, logger_freq)
 
-    if model_desc_file:
+    if model_desc_file and logdir:
         model_desc_save_path = os.path.join(logdir, model_desc_file)
         with open(model_desc_save_path, 'w') as f:
             f.write(found_model_desc.serialize())
@@ -77,3 +61,21 @@ def search_arch(conf_common:Config, conf_data:Config, conf_search:Config,
     return found_model_desc
 
 
+def create_model_desc(conf_data: Config, conf_model_desc: Config,
+                      dag_mutator: DagMutator) -> ModelDesc:
+    builder = ModelDescBuilder(
+        conf_data, conf_model_desc, run_mode=RunMode.Search)
+    model_desc = builder.get_model_desc()
+    dag_mutator.mutate(model_desc)
+    return model_desc
+
+def create_model(conf_data: Config, conf_model_desc: Config,
+                 dag_mutator: DagMutator, device) -> Model:
+    model_desc = create_model_desc(conf_data, conf_model_desc, dag_mutator)
+    model = Model(model_desc)
+    # if data_parallel:
+    #     model = nn.DataParallel(model).to(device)
+    # else:
+    # TODO: enable DataParallel
+    model = model.to(device)
+    return model
--- tests/cell_shape_test.py
+++ tests/cell_shape_test.py
@@ -1,3 +1,38 @@
+import torch
+
+from FastAutoAugment.common.common import common_init
+from FastAutoAugment.darts.darts_dag_mutator import DartsDagMutator
+from FastAutoAugment.nas.search_arch import create_model
+
 
 def test_cell_shape():
-    pass
+    conf = common_init(config_filepath=None,
+                       defaults_filepath='confs/defaults.yaml',
+                       experiment_name=None,
+                       param_args=['--common.logdir', None])
+
+    conf_data = conf['dataset']
+    conf_search = conf['darts']['search']
+    conf_model_desc = conf_search['model_desc']
+
+    dag_mutator = DartsDagMutator()
+    device = torch.device('cuda')
+
+    model = create_model(conf_data, conf_model_desc, dag_mutator, device)
+
+    x = torch.randn(64, 3, 32, 32).to(device)
+
+    s0 = model._stem0_op(x)
+    s1 = model._stem1_op(x)
+
+    assert list(s0.shape) == [64, 48, 32, 32]
+
+    logits_aux = None
+    for cell in model._cells:
+        s0, s1 = s1, cell.forward(s0, s1)
+        if cell.aux_tower is not None:
+            logits_aux = cell.aux_tower(s1)
+
+    # s1 is now the last cell's output
+    out = model.final_pooling(s1)
+    logits = model.linear(out.view(out.size(0), -1))  # flatten
