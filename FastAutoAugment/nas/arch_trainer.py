3a9ad659b6f31ed93af8a0a1b75c14d0e5cfa1cd
Shital Shah <sytelus@gmail.com>
2019-12-26 15:02:55 -0800

    arch_optimizer interface, remove bilevel option

--- FastAutoAugment/nas/arch_trainer.py
+++ FastAutoAugment/nas/arch_trainer.py
@@ -1,7 +1,9 @@
 from typing import Optional, Tuple, List
 from abc import ABC, abstractmethod
 
+from torch import Tensor
 from torch.utils.data import DataLoader
+from torch.optim.optimizer import Optimizer
 
 from overrides import EnforceOverrides
 
@@ -19,4 +21,10 @@ class ArchTrainer(ABC, EnforceOverrides):
             train_dl:DataLoader, val_dl:Optional[DataLoader],
             epochs:int, plotsdir:str, logger_freq:int)\
                 ->Tuple[ModelDesc, Metrics, Optional[Metrics]]:
+        pass
+
+class ArchOptimizer(ABC, EnforceOverrides):
+    @abstractmethod
+    def step(self, x_train:Tensor, y_train:Tensor, x_valid:Tensor, y_valid:Tensor,
+            optim:Optimizer, train_metrics:Metrics)->None:
         pass
