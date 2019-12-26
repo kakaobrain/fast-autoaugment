cc74d9d8d27ce626440caf4a72140e19c3073641
Shital Shah <shitals@microsoft.com>
2019-12-26 11:06:01 -0800

    temp

---
+++ scripts/petridish/cifar_search.py
@@ -0,0 +1,17 @@
+from FastAutoAugment.darts.darts_arch_trainer import DartsArchTrainer
+from FastAutoAugment.common.common import common_init
+from FastAutoAugment.nas.search_arch import search_arch
+from FastAutoAugment.petridish.petridish_mutator import PetridishMutator
+
+if __name__ == '__main__':
+    conf = common_init(defaults_filepath='confs/defaults.yaml',
+                       experiment_name='cifar_search')
+
+    conf_common = conf['common']
+    conf_data = conf['dataset']
+    conf_search = conf['darts']['search']
+
+    dag_mutator = PetridishMutator()
+    arch_trainer = DartsArchTrainer()
+    found_model_desc = search_arch(conf_common, conf_data, conf_search,
+                                   dag_mutator, arch_trainer)
---
+++ scripts/petridish/cifar_test.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+
+from FastAutoAugment.common.common import common_init
+from FastAutoAugment.nas.test_arch import test_arch
+
+
+if __name__ == '__main__':
+    conf = common_init(defaults_filepath='confs/defaults.yaml',
+                       experiment_name='cifar_test')
+
+    conf_common = conf['common']
+    conf_data         = conf['dataset']
+    conf_test       = conf['darts']['test']
+
+    best_top1, model = test_arch(conf_common, conf_data, conf_test)
+
