a0e19e225db4c5374d9031baa593af3367e732e8
Debadeepta Dey <dedey@microsoft.com>
2019-12-29 03:59:10 +0000

    Cleaned up random dag mutator.

--- FastAutoAugment/random_arch/random_dag_mutator.py
+++ FastAutoAugment/random_arch/random_dag_mutator.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, Sequence, Tuple, Dict, Optional, List
+from typing import Callable, Iterable, Sequence, Tuple, Dict, Optional, List, Set
 import random
 
 from torch.utils.data import DataLoader
@@ -34,6 +34,27 @@ class RandomDagMutator(DagMutator):
     def __init__(self) -> None:        
         self.op_desc_container = OpDescContainer()
 
+    def _sample_without_replacement(self, low:int, high:int, num_to_sample:int) -> Set[int]:
+        assert high - low + 1 >= num_to_sample
+
+        connect_ids = set()
+        while len(connect_ids) < num_to_sample:
+            state_id = random.randint(low, high)
+            connect_ids.add(state_id)
+
+        return connect_ids
+
+    def _sample_ops(self, num_to_sample:int) -> List[str]:
+
+        ops_selected = []
+        for i in range(num_to_sample):
+            p_ind = random.randint(0, len(RandomDagMutator.PRIMITIVES)-2)
+            p_name = RandomDagMutator.PRIMITIVES[p_ind]
+            ops_selected.append(p_name)
+
+        return ops_selected
+        
+
     @overrides
     def mutate(self, model_desc:ModelDesc)->None:
         
@@ -55,36 +76,29 @@ class RandomDagMutator(DagMutator):
         # TODO: Shouldn't run_mode be a property of the 
         # entire model_desc instead of each cell?
         run_mode = model_desc.cell_descs[-1].run_mode
+
+        # Number of edges going in to a state
+        # TODO: Make into a parameter
+        num_incoming = 2
         
         for i in range(num_nodes):
             
-            # Every node connects to randomly chosen two 
-            # previous states via a random operation
-            connect_state_0 = random.randint(0, i+1)
-            # TODO: Make sure that you are not 
-            # connecting to the same state
-            connect_state_1 = random.randint(0, i+1)
+            connect_ids = self._sample_without_replacement(0, i+1, num_incoming)
 
-            p_ind_0 = random.randint(0, len(RandomDagMutator.PRIMITIVES)-2)
-            p_0_name = RandomDagMutator.PRIMITIVES[p_ind_0]
-
-            p_ind_1 = random.randint(0, len(RandomDagMutator.PRIMITIVES)-2)
-            p_1_name = RandomDagMutator.PRIMITIVES[p_ind_1]
-            
-            params_0 = {}
-            params_0['source_state'] = connect_state_0
-
-            params_1 = {}
-            params_1['source_state'] = connect_state_1
+            op_names = self._sample_ops(num_incoming)
 
             in_len = 1
-            op_desc_0 = OpDesc(p_0_name, run_mode, params_0, in_len)
-            op_desc_1 = OpDesc(p_1_name, run_mode, params_1, in_len)
+            op_descs = []
+            for id_and_op in zip(connect_ids, op_names):
+                params = {}
+                params['source_state'] = id_and_op[0]
+                op_desc = OpDesc(id_and_op[1], run_mode, params, in_len)
+                op_descs.append(op_desc)
 
-            self.op_desc_container.insert_op_desc(op_desc_0, op_desc_1)
-    
+            self.op_desc_container.insert_op_desc(tuple(op_descs))
+            
         # Now mutate every cell according to the
-        # randomly sampled template
+        # random template
         #------------------------------------
         for cell_desc in model_desc.cell_descs:
             self._mutate_cell(cell_desc)
@@ -94,34 +108,24 @@ class RandomDagMutator(DagMutator):
         ch_out = cell_desc.n_node_channels
         reduction = (cell_desc.cell_type==CellType.Reduction)
 
-        # add random op for each edge
+        # Add random op for each edge
         for i, node in enumerate(cell_desc.nodes):
-            # Every node has two incoming edges
-            op_desc_0, op_desc_1 = self.op_desc_container[i]
+            # Every node has K incoming edges
+            op_descs = self.op_desc_container[i]
+        
+            for op_desc in op_descs:
+                op_desc.params['ch_in'] = ch_out
+                op_desc.params['ch_out'] = ch_out
+                op_desc.params['stride'] = 2 if reduction and op_desc.params['source_state'] < 2 else 1
+                op_desc.params['affine'] = cell_desc!=RunMode.Search    
 
-            op_desc_0.params['ch_in'] = ch_out
-            op_desc_0.params['ch_out'] = ch_out
-            op_desc_0.params['stride'] = 2 if reduction and op_desc_0.params['source_state'] < 2 else 1
-            op_desc_0.params['affine'] = cell_desc!=RunMode.Search
-
-            op_desc_1.params['ch_in'] = ch_out
-            op_desc_1.params['ch_out'] = ch_out
-            op_desc_1.params['stride'] = 2 if reduction and op_desc_1.params['source_state'] < 2 else 1
-            op_desc_1.params['affine'] = cell_desc!=RunMode.Search
-
-            edge_0 = EdgeDesc(op_desc_0, len(node.edges),
-                                input_ids=[op_desc_0.params['source_state']],
+                edge = EdgeDesc(op_desc, len(node.edges),
+                                input_ids=[op_desc.params['source_state']],
                                 from_node=-1,
                                 to_state=-1)
 
-            node.edges.append(edge_0)
+                node.edges.append(edge)
 
-            edge_1 = EdgeDesc(op_desc_1, len(node.edges),
-                                input_ids=[op_desc_1.params['source_state']],
-                                from_node=-1,
-                                to_state=-1)
-
-            node.edges.append(edge_1)
 
            
 
