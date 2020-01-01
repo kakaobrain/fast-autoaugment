f39d665d9512be2a362485801768bccfa10d2f41
Debadeepta Dey <dedey@microsoft.com>
2020-01-01 01:03:59 +0000

    Updated docker with missing package.

--- docker/test_docker_locally.sh
+++ docker/test_docker_locally.sh
@@ -1,3 +1,3 @@
 # This command maps /home/dedey to /root/dedey inside the docker and drops one into 
 # a terminal inside the docker. Then one can just run the training command.
-docker run --runtime=nvidia -it --rm -v /home/dedey:/root/dedey debadeepta/petridishpytorch 
+docker run --runtime=nvidia -it --rm -v /home/dedey:/root/dedey debadeepta/petridishpytorch:latest 
